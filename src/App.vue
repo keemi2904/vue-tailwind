ec1895547a8be73332e6e42cc9cf3b0b1a45c553
HakeemAzeez <22ftt1511@student.pb.edu.bn>
2024-04-15 19:07:57 +0800

    added image locally from assets

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import MyImage from "./assets/image1.png";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
   <h1 class="text-3xl font-bold underline">Hello</h1>
+  <img :src="MyImage"></img>
 </template>
 
 <style scoped>
